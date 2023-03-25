b71761758450abd8aefd4328875b4611b2e62d88
Karim Ali <kareem.ali.ali.99@gmail.com>
2023-03-25 21:03:35 +0100

    Update model.py

--- llama/model.py
+++ llama/model.py
@@ -20,10 +20,10 @@ from fairscale.nn.model_parallel.layers import (
 @dataclass
 class ModelArgs:
     dim: int = 512
-    n_layers: int = 8
-    n_heads: int = 8
+    n_layers: int = 1 # 8
+    n_heads: int = 2 # 8
     vocab_size: int = -1  # defined later by tokenizer
-    multiple_of: int = 256  # make SwiGLU hidden layer size multiple of large power of 2
+    multiple_of: int = 4 # 256  # make SwiGLU hidden layer size multiple of large power of 2
     norm_eps: float = 1e-5
 
     max_batch_size: int = 32
